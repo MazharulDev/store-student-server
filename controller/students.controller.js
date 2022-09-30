fffad40565767bf52eeea76f3cfed31977861403
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-09-30 10:43:26 +0600

    get and delete api created

--- controller/students.controller.js
+++ controller/students.controller.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const { getDb } = require("../utils/dbConnect");
 
 module.exports.addStudent = async (req, res) => {
@@ -6,6 +7,26 @@ module.exports.addStudent = async (req, res) => {
         const students = req.body;
         const result = await db.collection("student").insertOne(students);
         res.send(result)
+    } catch (error) {
+
+    }
+};
+module.exports.saveStudent = async (req, res) => {
+    try {
+        const db = getDb();
+        const student = await db.collection("student").find().toArray();
+        res.send(student)
+    } catch (error) {
+
+    }
+}
+module.exports.deleteStudent = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const db = getDb()
+        const query = { _id: ObjectId(id) };
+        const result = db.collection("student").deleteOne(query);
+        res.send(result);
     } catch (error) {
 
     }
--- routes/students.route.js
+++ routes/students.route.js
@@ -3,6 +3,8 @@ const studentsController = require("../controller/students.controller");
 const router = express.Router();
 
 router.post('/', (studentsController.addStudent))
+router.get('/', studentsController.saveStudent)
+router.delete('/:id', studentsController.deleteStudent)
 
 
 module.exports = router;
